e0f248ebea7ba9bd9a401757d4f1edade44a58a1
Aleksandar Vikor <vikor.aleksandar@gmail.com>
2020-11-17 13:27:57 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div className='App'>Hello react</div>;
+	return <div className='App container'>Hello react</div>;
 }
 
 export default App;
